a7392a5442612da8fe7ecd54dc7ce10751bfbf3e
Chuck Danger <beneth@gmail.com>
2017-12-09 02:53:57 -0600

    blah
    blah

--- poshrsjob_example.ps1
+++ poshrsjob_example.ps1
@@ -1,19 +1,33 @@
 ﻿# Set a value to pass into script block
 $test = 'test'
-1..10 | Start-RSJob -Throttle 5 -ScriptBlock {
+1..50 | Start-RSJob -Throttle 10 -ScriptBlock {
+
+    $seconds = Get-Random (1..5)
     # Pull in var from outside the scriptblock
     [pscustomobject]@{
-        Test=$Using:test
+        Job="$Using:test"+"$_"
+        Seconds=$seconds
     }
-    $seconds = Get-Random (1..5)
     Start-Sleep -Seconds $seconds
-    Write-Output "Job $Using:test-$_ slept for $seconds seconds."
+    #Write-Output "Job $Using:test-$_ slept for $seconds seconds."
 # Waits for all jobs to complete before outputting results with jobs in numerical order
 #} | Wait-RSJob | Get-RSJob | Receive-RSJob
 
 # Outputs each job's info as it completes
-} | Wait-RSJob -Timeout 8 -ShowProgress | Receive-RSJob
+#} | Wait-RSJob -Timeout 8 -ShowProgress | Receive-RSJob
+}
 
-Get-RSJob
+do {
+$runningcount = (Get-RSJob | ? {$_.state -eq "Running"}).count
+$remaining = (Get-RSJob | ? {$_.state -eq "NotStarted"}).count
+Write-Host "$runningcount threads running, $remaining threads not started."
+Start-Sleep -Seconds 5
+} until ($runningcount -eq 0)
+Write-Host "All threads complete"
 
-Get-RSJob | Remove-RSJob -Verbose
+$rsObjs = @()
+$rsObjs = Get-RSJob | Receive-RSJob
+Get-RSJob | Remove-RSJob
+$rsObjs | Export-CSV -Path "$PSScriptRoot\test.csv" -NoTypeInformation
+
+#Get-RSJob | Remove-RSJob -Verbose
